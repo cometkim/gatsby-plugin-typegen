5abcbdb5163ef109cb504d8faadf3060d8aab15f
John Rom <john@nimblelight.com>
2020-02-20 12:35:31 -0500

    Export Fragment Subtypes

--- src/workers/codegen.ts
+++ src/workers/codegen.ts
@@ -72,7 +72,12 @@ export const setupCodegenWorker: SetupCodegenWorkerFn = ({
       codegenOptions.pluginMap['typescript'] = require('@graphql-codegen/typescript');
       codegenOptions.plugins.push({ typescript: DEFAULT_TYPESCRIPT_CONFIG });
       codegenOptions.pluginMap['typescriptOperations'] = require('@graphql-codegen/typescript-operations');
-      codegenOptions.plugins.push({ typescriptOperations: DEFAULT_TYPESCRIPT_CONFIG });
+      codegenOptions.plugins.push({
+        typescriptOperations: {
+          ...DEFAULT_TYPESCRIPT_CONFIG,
+          exportFragmentSpreadSubTypes: true
+        }
+      });
       if (includeResolvers) {
         codegenOptions.pluginMap['typescriptResolvers'] = require('@graphql-codegen/typescript-resolvers');
         codegenOptions.plugins.push({
